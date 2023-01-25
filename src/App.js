e311acb40946001b84b3bbb0d38c4c24fba26070
matiascalegaris <calegaris.matias@gmail.com>
2023-01-25 16:30:38 -0300

    add api callbacks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.scss';
 import Loading from './Components/Dialogs/Loading/loading';
 import LogInFlow from './Components/Login-flow/login-flow';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    console.log(`app loaded`);
+    window.parent.APicallbacks = {
+      ErrorMessage: (msg, msgType, action) => {
+        console.log("got a message: " + msg + ", msg type: " + msgType + " and action: " + action)
+      }
+    }
+  },[]);
   return (
     <div className='app'>
       <LogInFlow/>
