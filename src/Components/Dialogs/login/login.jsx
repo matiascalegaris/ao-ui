22dec432e7853d16d5578571c2867e20f13d5cb2
matiascalegaris <calegaris.matias@gmail.com>
2023-01-24 22:08:46 -0300

    add calls to new C api

--- src/Components/Dialogs/login/login.jsx
+++ src/Components/Dialogs/login/login.jsx
@@ -7,8 +7,8 @@ import AoDialog from '../../Common/ao-dialog/ao-dialog';
 import AoCheckbox from "../../Common/ao-checkbox/ao-checkbox";
 
 export default function LogIn({setNewDialog}) {
-  const [userCredentials, setCredentials] = useState({email:'', password:''});
-  const {email, password } = userCredentials;
+  const [userCredentials, setCredentials] = useState({email:'', password:'', storeCredentials:false});
+  const {email, password, storeCredentials } = userCredentials;
   const { t } = useTranslation();
 
   const handleChange = event => {
@@ -18,6 +18,15 @@ export default function LogIn({setNewDialog}) {
   const updateDialog = event => {
     event.preventDefault();
     setNewDialog('create-account')
+  }
+  const DoLogin = event => {
+    event.preventDefault();
+    window.parent.BabelUI.Login(email, password);
+  }
+
+  const DoClose = event => {
+    event.preventDefault();
+    window.parent.BabelUI.CloseClient();
   }
   return (
     <AoDialog styles='login-dialog-pos'>
@@ -36,11 +45,11 @@ export default function LogIn({setNewDialog}) {
             <AoInput name="password" type="password" value={password} required handleChange={handleChange} />
           </div>
           <AoCheckbox label={t('Remember me')} styles='split-area right-padding'/>
-          <AoButton caption='connect' styles='split-area red-bg'/>
+          <AoButton caption='connect' styles='split-area red-bg'  onClick={ DoLogin }/>
       </div>
       <div class='bottom-line'>
         <AoButton caption='account' styles='split-area' onClick={ updateDialog }/>
-        <AoButton caption='exit' styles='split-area'/>
+        <AoButton caption='exit' styles='split-area' onClick={ DoClose }/>
       </div>
     </AoDialog>
   )
